50b30ea8bc8f32bb98ce4b040c216a5a3332bb3a
Luke Reynolds <lukereynoldspi@gmail.com>
2022-03-17 19:41:50 -0700

    added test to tile spec for attribute

--- spec/tile_spec.rb
+++ spec/tile_spec.rb
@@ -5,14 +5,15 @@ describe 'A tile' do
   describe 'attributes' do
 
     it 'has a value' do
-      skip
+      tile = Tile.new(1)
+      tile.value
     end
 
   end
 
   describe '#up?' do
     it 'is up by default' do
-      skip
+      tile = Tile.new
     end
   end
 
